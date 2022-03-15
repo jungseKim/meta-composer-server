7311d251bad02e68c2ea47ff766f1c42d0e720d4
jungseKim <80089414+jungseKim@users.noreply.github.com>
2022-03-15 13:48:16 +0900

    다시 오류수정

--- src/modules/notification/notification.module.ts
+++ src/modules/notification/notification.module.ts
@@ -3,11 +3,13 @@ https://docs.nestjs.com/modules
 */
 
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from 'src/entities/user.entity';
 import { NotificationGateway } from './notification.gateway';
 import { NotificationService } from './notification.service';
 
 @Module({
-  imports: [],
+  imports: [TypeOrmModule.forFeature([User])],
   controllers: [],
   providers: [NotificationGateway, NotificationService],
 })
