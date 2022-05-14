4fd23cf54b18a3395bace82930d71acca67cfb9f
jungseKim <80089414+jungseKim@users.noreply.github.com>
2022-05-14 16:33:24 +0900

    orderby api and swagger

--- src/modules/wishlists/dto/wishlist.dto.ts
+++ src/modules/wishlists/dto/wishlist.dto.ts
@@ -2,7 +2,6 @@ import { PickType } from "@nestjs/swagger";
 import { Wishlist } from "src/entities/wishlist.entity";
 
 export class WishListDto extends PickType(Wishlist, [
- 'userId',
-  'lessonId'
- 
-]as const){}
+  "userId",
+  "lessonId",
+] as const) {}
